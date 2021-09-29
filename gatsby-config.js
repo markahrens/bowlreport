a365a369307e0c474da06afa521ad010205c6d64
Mark Ahrens <mark.ahrens@acquia.com>
2021-09-29 14:18:44 -0500

    updating packages, headers, theme color.

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2a8399`,
+        theme_color: `#2a8399`,
         display: `minimal-ui`,
         icon: `src/images/football-icon.png`, // This path is relative to the root of the site.
       },
